420e34e381b5defedc6569add95f243984dc6fdc
Etor <etorarza@gmail.com>
2020-01-28 13:06:50 +0100

    Fixed bufg in hip_permus script

--- src/experiments/permus/scripts/hip_train_all_qap_controllers.sh
+++ src/experiments/permus/scripts/hip_train_all_qap_controllers.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 
-for problem in "qap" "tsp" "lop" "pfsp"
+for problem in "qap" "tsp" "lop" "pfsp"; do
 for instance in src/experiments/permus/instances/${problem}/*; do
-    sbatch scripts/exec_hipatia_train.sh "$problem" "$instance" 0.25
+    sbatch src/experiments/permus/scripts/exec_hipatia_train.sh "$problem" "$instance" 0.25
 done
 done
 
 
 for instance in src/experiments/permus/instances/qap/cut_instances/*; do
-    sbatch scripts/exec_hipatia_train.sh "qap" "$instance" 0.25
+    sbatch src/experiments/permus/scripts/exec_hipatia_train.sh "qap" "$instance" 0.25
 done
 
 
