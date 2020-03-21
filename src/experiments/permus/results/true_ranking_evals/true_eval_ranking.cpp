f5ce9c898b4dfc1b0bd7d0dc79499d7aab02a040
Etor Arza <etorarza@gmail.com>
2020-03-21 13:39:12 +0100

    add true_ranking_experiment files

---
+++ src/experiments/permus/results/true_ranking_evals/true_eval_ranking.cpp
@@ -0,0 +1,54 @@
+#include "true_eval_ranking.h"
+#include "cpunetwork.h"
+// #include "FitnessFunction_permu.h"
+// #include "experiment.h"
+// #include "genomemanager.h"
+// #include "network.h"
+// #include "organism.h"
+// #include "population.h"
+// #include "stats.h"
+// #include "timer.h"
+// #include "util.h"
+// #include "Parameters.h"
+// #include <chrono>
+// #include <float.h>
+// #include "evaluatorexperiment.h"
+// #include "rng.h"
+// #include "permuevaluator.cxx"
+
+
+void get_algorithm_ranking(NEAT::CpuNetwork **array_of_networks, int n_of_evals, int seed, int array_size, int *ranking)
+{
+}
+
+void true_eval_ranking_experiment()
+{
+
+    using namespace NEAT;
+    #define seed 4
+
+    // rng_t rng_exp(seed);
+    // class Population *pop;
+
+    // auto ev = PermuEvaluator();
+
+    // PERMU::params *parameters;
+    // NetworkExecutor<PERMU::Evaluator> *executor;
+
+    // executor = NEAT::NetworkExecutor<PERMU::Evaluator>::create();
+    // parameters = new PERMU::params();
+
+
+    // EvaluatorExperiment ev_ = ;
+    
+    // EvaluatorExperiment("permu", create_evaluator, create_seeds);
+    // PermuEvaluator *ev = (PermuEvaluator *) & ev_;
+
+    // ev->read_conf_file("src/experiments/permus/results/true_ranking_evals/test.ini");
+
+
+    // double res = PERMU::FitnessFunction_permu(nets[0], 3, seed, ev->pointer_to_parameters());
+
+     // cout << res << " <- score";
+
+}
---
+++ src/experiments/permus/results/true_ranking_evals/true_eval_ranking.h
@@ -0,0 +1,5 @@
+#include "cpunetwork.h"
+
+void get_algorithm_ranking(NEAT::CpuNetwork **array_of_networks, int n_of_evals, int seed, int array_size, int* ranking);
+
+void true_eval_ranking_experiment();
