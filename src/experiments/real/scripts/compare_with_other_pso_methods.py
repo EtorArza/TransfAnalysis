1bccc134ba13ec1df491588d52a251fe09998005
EtorArza <etorarza@gmail.com>
2021-02-01 14:01:56 +0100

    n repetitions in csv compare other pso methods

--- src/experiments/real/scripts/compare_with_other_pso_methods.py
+++ src/experiments/real/scripts/compare_with_other_pso_methods.py
@@ -3,11 +3,10 @@ import subprocess
 
 CONTROLLER_PATH = "src/experiments/real/results/comparison_other_pso/controllers_trained_on_all_problems/top_controllers/"
 N_EVALS = 30
-N_REPS = 1
 THREADS = 7
 
 
-def write_conf_file(METHOD_SHORTNAME,SOLVER_POPSIZE,PROBLEM_INDEX,PROBLEM_DIM,MAX_SOLVER_FE,X_LOWER_LIM,X_UPPER_LIM,F_COMPETITION):
+def write_conf_file(METHOD_SHORTNAME,SOLVER_POPSIZE,PROBLEM_INDEX,PROBLEM_DIM,MAX_SOLVER_FE,X_LOWER_LIM,X_UPPER_LIM,F_COMPETITION, N_REPS):
 
     values = {
         "SOLVER_POPSIZE":SOLVER_POPSIZE,
@@ -55,15 +54,15 @@ X_UPPER_LIM = {X_UPPER_LIM}
         f.writelines(tmp_ini_file_string)
     
 results = []
-def record_results(METHOD_SHORTNAME,SOLVER_POPSIZE,PROBLEM_INDEX,PROBLEM_DIM,MAX_SOLVER_FE,X_LOWER_LIM,X_UPPER_LIM,F_COMPETITION):
+def record_results(METHOD_SHORTNAME,SOLVER_POPSIZE,PROBLEM_INDEX,PROBLEM_DIM,MAX_SOLVER_FE,X_LOWER_LIM,X_UPPER_LIM,F_COMPETITION, N_REPS):
     write_conf_file(METHOD_SHORTNAME, SOLVER_POPSIZE, PROBLEM_INDEX,
-                    PROBLEM_DIM, MAX_SOLVER_FE, X_LOWER_LIM, X_UPPER_LIM, F_COMPETITION)
+                    PROBLEM_DIM, MAX_SOLVER_FE, X_LOWER_LIM, X_UPPER_LIM, F_COMPETITION, N_REPS)
     subprocess.run("./neat tmp_conf_file.ini > /dev/null",shell=True)
     # subprocess.run("./neat tmp_conf_file.ini",shell=True)
     with open("result.txt","r") as f:
         res = f.readline().strip()
     res_list = eval("["+str(F_COMPETITION)+","+res+"]") # file="coparison_result.txt")
-    print(METHOD_SHORTNAME, MAX_SOLVER_FE, SOLVER_POPSIZE, (X_LOWER_LIM,X_UPPER_LIM), "{:.4e}".format(res_list[0]), "{:.4e}".format(-res_list[1][0][0]), res_list[1][1], PROBLEM_DIM, sep=",")
+    print(METHOD_SHORTNAME, MAX_SOLVER_FE, SOLVER_POPSIZE, (X_LOWER_LIM,X_UPPER_LIM), "{:.4e}".format(res_list[0]), "{:.4e}".format(-res_list[1][0][0]), res_list[1][1], PROBLEM_DIM, N_REPS, sep=",")
     results.append([METHOD_SHORTNAME, MAX_SOLVER_FE, SOLVER_POPSIZE, (X_LOWER_LIM,X_UPPER_LIM), "{:.4e}".format(res_list[0]), "{:.4e}".format(-res_list[1][0][0]), res_list[1][1], PROBLEM_DIM])
     subprocess.run("rm result.txt",shell=True)
 
@@ -107,6 +106,7 @@ for row_index, row in df.iterrows():
         PROBLEM_DIM = int(row[0].split("|")[0])
         MAX_SOLVER_FE = int(row[0].split("|")[1])
         SOLVER_POPSIZE = int(row[0].split("|")[2])
+        N_REPS = int(row[0].split("|")[3])
         for col_index in range(1,len(row)):
             PROBLEM_INDEX = col_index # first problem is in col 1, and the problem index is 1 too.
             if  df.isnull().iloc[(problem_lims_row_index,PROBLEM_INDEX)]:
@@ -115,7 +115,7 @@ for row_index, row in df.iterrows():
             X_UPPER_LIM = float(df.iloc[(problem_lims_row_index,PROBLEM_INDEX)].split("|")[1])
             F_COMPETITION = float(df.iloc[(row_index,PROBLEM_INDEX)])
 
-            record_results(METHOD_SHORTNAME,SOLVER_POPSIZE,PROBLEM_INDEX,PROBLEM_DIM,MAX_SOLVER_FE,X_LOWER_LIM,X_UPPER_LIM,F_COMPETITION)
+            record_results(METHOD_SHORTNAME,SOLVER_POPSIZE,PROBLEM_INDEX,PROBLEM_DIM,MAX_SOLVER_FE,X_LOWER_LIM,X_UPPER_LIM,F_COMPETITION, N_REPS)
     
 
 for i in range(len(results)):
