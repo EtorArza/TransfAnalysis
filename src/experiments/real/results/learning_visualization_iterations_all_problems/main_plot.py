5dc6d13ebe0d2e3a8fe66d3467338fbbf2fd5a33
Etor Arza <etorarza@gmail.com>
2022-02-07 12:40:14 +0000

    plotting generations score almost working

--- src/experiments/real/results/learning_visualization_iterations_all_problems/main_plot.py
+++ src/experiments/real/results/learning_visualization_iterations_all_problems/main_plot.py
@@ -5,42 +5,42 @@ from tqdm import tqdm as tqdm
 from sklearn.preprocessing import StandardScaler
 
 SEED=2
-SOLVER_POPSIZE=20
-MAX_SOLVER_FE=200000
-DIM=10
+SOLVER_POPSIZE=10
+MAX_SOLVER_FE=1000
 N_EVALS = 100 # N of repetitions averaged
 N_REPS = 1 # N of averaged repetitions repeated
-THREADS = 16
-CONTROLLER_PATH = 'src/experiments/real/results/learning_visualization_iterations_all_problems/all_controllers/'
-CONTROLLER_PREFIX = 'trained_with_all_problems_true'
-N_PROBLEMS = 10
-FULL_MODEL = "true"
+THREADS = 1
+CONTROLLER_PATH = '/home/paran/Dropbox/BCAM/02_NEAT_transferability/code/NEAT_code/experimentResults/transfer_generated_continuous/controllers/all_controllers'
+FULL_MODEL = "false"
 
 
-instance_list=[1,2,3,4,5,6,7,8,9,10]
-lower_list=[-10,-10,-5,-5.12,-15,-600,-5,-15,-100,-2.048]
-upper_list=[5,10,10,5,30,600,10,30,100,2.048]
-dim_list = [10]*10
+instance_index_list=[0,0,0]
+dim_list = [20,20,20]
+NLO_train_list = [2,4,6]
+NLO_test_list = [4,4,4]
+CONTROLLER_PREFIX_list = [f'NLO_{NLO}' for NLO in NLO_train_list]
 
-def write_conf_file(SOLVER_POPSIZE,PROBLEM_INDEX,PROBLEM_DIM,X_LOWER_LIM,X_UPPER_LIM,GEN_INDEX):
 
-    values = {
-        "SOLVER_POPSIZE":SOLVER_POPSIZE,
-        "PROBLEM_INDEX":PROBLEM_INDEX,
-        "PROBLEM_DIM":PROBLEM_DIM,
-        "MAX_SOLVER_FE":MAX_SOLVER_FE,
-        "X_LOWER_LIM":X_LOWER_LIM,
-        "X_UPPER_LIM":X_UPPER_LIM,
-        "CONTROLLER_PATH":CONTROLLER_PATH,
-        "N_EVALS":N_EVALS,
-        "N_REPS":N_REPS,
-        "THREADS":THREADS,
-        "GEN_INDEX":str(GEN_INDEX).zfill(9),
-        "FULL_MODEL": FULL_MODEL,
-        "CONTROLLER_PREFIX": CONTROLLER_PREFIX,
-    }
+def write_conf_file(SOLVER_POPSIZE,PROBLEM_INDEX,PROBLEM_DIM,GEN_INDEX, CONTROLLER_PREFIX, NLO, SEED):
+    subprocess.run(f"""
+mkdir -p src/experiments/real/real_func_src/jani_ronkkonen_problem_generator/
+cat > src/experiments/real/real_func_src/jani_ronkkonen_problem_generator/quad_function.dat <<EOF
+1
+1
+{PROBLEM_DIM}
+{SEED}
+-1
+1
+{NLO}
+-1
+-1
+-1
+-1
+EOF        
+""",shell=True)
 
-    tmp_ini_file_string = """
+    
+    tmp_ini_file_string = f"""
 [Global] 
 mode = test
 PROBLEM_NAME = real_func
@@ -62,49 +62,51 @@ PROBLEM_INDEX = {PROBLEM_INDEX}
 PROBLEM_DIM = {PROBLEM_DIM}
 MAX_SOLVER_FE = {MAX_SOLVER_FE}
 
-X_LOWER_LIM = {X_LOWER_LIM}
-X_UPPER_LIM = {X_UPPER_LIM}
-""".format(**values)
-
+"""
     with open("tmp_conf_file.ini","w") as f:
         f.writelines(tmp_ini_file_string)
 
 
 
-def return_results(SOLVER_POPSIZE,PROBLEM_INDEX,PROBLEM_DIM,X_LOWER_LIM,X_UPPER_LIM,GEN_INDEX):
-    write_conf_file(SOLVER_POPSIZE,PROBLEM_INDEX,PROBLEM_DIM,X_LOWER_LIM,X_UPPER_LIM,GEN_INDEX)
-    subprocess.run("./neat tmp_conf_file.ini > /dev/null",shell=True)
-    # subprocess.run("./neat tmp_conf_file.ini",shell=True)
+def return_results(SOLVER_POPSIZE,PROBLEM_INDEX,PROBLEM_DIM,GEN_INDEX, CONTROLLER_PREFIX, NLO, SEED):
+    write_conf_file(SOLVER_POPSIZE,PROBLEM_INDEX,PROBLEM_DIM,GEN_INDEX, CONTROLLER_PREFIX, NLO, SEED)
+    #subprocess.run("./neat tmp_conf_file.ini > /dev/null",shell=True)
+    subprocess.run("./neat tmp_conf_file.ini",shell=True)
     with open("score.txt","r") as f:
         res = f.readline().strip()
+    print(SOLVER_POPSIZE,PROBLEM_INDEX,PROBLEM_DIM,GEN_INDEX, CONTROLLER_PREFIX, NLO, SEED)
+    print(res)
     subprocess.run("rm score.txt",shell=True)
     return eval(res)[0][0]
 
+j = -1
+for instance_index, NLO, dim in zip(instance_index_list,NLO_train_list,dim_list):
+    j += 1
 
-files = str(subprocess.run('ls '+CONTROLLER_PATH,shell=True, capture_output=True).stdout).split(r"\n")
-generations = [int(el.split("_gen_")[-1].split(".controller")[0]) for el in files if len(el) > 6]
+    nlo_train = NLO_train_list[j]
+    NLO_test = NLO_test_list[j]
+    controller_prefix = CONTROLLER_PREFIX_list[j]
+    dim = dim_list[j]
 
-
-
-results = np.zeros((len(generations), len(instance_list)))
-for i, gen in enumerate(tqdm(generations)):
-    for j, problem_index in enumerate(instance_list):
-        X_LOWER_LIM = lower_list[j]
-        X_UPPER_LIM = upper_list[j]
-        DIM = dim_list[j]
-
-        
-        results[i,j] = return_results(SOLVER_POPSIZE, problem_index, DIM, X_LOWER_LIM, X_UPPER_LIM, gen)
+    files = str(subprocess.run('ls '+CONTROLLER_PATH + f"/NLO_{nlo_train}_*",shell=True, capture_output=True).stdout).split(r"\n")
+    generations = [str(el.split("_gen_")[-1].split(".controller")[0]) for el in files if len(el) > 6]
 
 
-results = StandardScaler().fit_transform(results)
+    print(generations)
 
-for column, label in zip(results.T, instance_list):
-    print(column, np.array(generations))
-    plt.plot(np.array(generations), column, label="F"+str(label))
+    results = np.zeros((len(generations), len(instance_index_list)))
+    for i, gen in enumerate(tqdm(generations)):
+        results[i,j] = return_results(SOLVER_POPSIZE, instance_index, dim, gen, controller_prefix, NLO_test, SEED)
 
 
-plt.legend()
-plt.show()
+    results = StandardScaler().fit_transform(results)
 
-print(results)
+    for column, label in zip(results.T, instance_index_list):
+        print(column, np.array([int(el) for el in generations]))
+        plt.plot(np.array([int(el) for el in generations]), column, label="F"+str(label))
+
+
+    plt.legend()
+    plt.show()
+
+    print(results)
