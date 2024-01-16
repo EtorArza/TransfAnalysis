ac2a3546802bdea727605db262a753509ec740a2
Etor Arza <etorarza@gmail.com>
2024-01-16 20:17:10 +0100

    ELA for continuous and rokkonen

--- experimentScripts/exploratory_landscape_analisys.R
+++ experimentScripts/exploratory_landscape_analisys.R
@@ -5,17 +5,7 @@
 library(flacco)
 
 
-get_features_problem <- function(problem_index) {
-
-
-  f_continuous <- function(x) {
-    x_str <- paste(x, collapse = ",")
-    cmd <- paste(c("./main.out -evaluate-continuous-problem", problem_index, x_str), collapse = " ")
-    res <- system(cmd, intern = TRUE)
-    return(as.double(res))
-  }
-
-
+get_features_problem <- function(objective_function) {
 
   ## (1) Create some example-data
   X = createInitialSample(n.obs = 500, dim = 20)
@@ -37,13 +27,49 @@ get_features_problem <- function(problem_index) {
 
 }
 
-
+# 12 continuous problems
+lines = c()
 for (i in 1:12) {
+  f_continuous <- function(x) {
+    x_str <- paste(x, collapse = ",")
+    cmd <- paste(c("./main.out -evaluate-continuous-problem", i, x_str), collapse = " ")
+    res <- system(cmd, intern = TRUE)
+    return(as.double(res))
+  }
+
   print(paste("Working on problem",i))
-  writeLines(paste(formatC(get_features_problem(i), format = "f", digits = 36), collapse = ","), paste("experimentResults/problem_analisys/exploratory_landscape_analisys/ELA_continuous_",i,".txt", collapse=""))
+  lines = c(lines,paste(paste("F_",i,",",sep="",collapse=""),paste(formatC(get_features_problem(i), format = "f", digits = 36), collapse = ",", sep=",")))
+}
+writeLines(lines, "experimentResults/problem_analisys/continuous12_ELA.txt")
+
+
+# generated continuous problems
+
+updateParameterInFile <- function(filePath, newNLO) {
+  fileContent <- readLines(filePath)
+  fileContent[7] <- as.character(newNLO)
+  writeLines(fileContent, filePath)
 }
 
 
+lines = c()
+for (nlo in c(1,4,8,16,32,64)) {
+  f_continuous <- function(x) {
+
+    updateParameterInFile("src/experiments/real/real_func_src/jani_ronkkonen_problem_generator/quad_function.dat", nlo)
+
+
+    x_str <- paste(x, collapse = ",")
+
+    cmd <- paste(c("./main.out -evaluate-continuous-problem", 0, x_str), collapse = " ")
+    res <- system(cmd, intern = TRUE)
+    return(as.double(res))
+  }
+
+  print(paste("Working on generated rokonen problem with NLO=",nlo))
+  lines = c(lines,paste(paste("NLO_",nlo,",",sep="",collapse=""),paste(formatC(get_features_problem(i), format = "f", digits = 36), collapse = ",", sep=",")))
+}
+writeLines(lines, "experimentResults/problem_analisys/rokkonen_ELA.txt")
 
 
 
